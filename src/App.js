e2a66bfa7241c9435744d83b8e8086f3e6f67be4
rroell <roelliie@hotmail.nl>
2022-11-28 01:34:12 +0100

    Handle undo + redo

--- src/App.js
+++ src/App.js
@@ -4,21 +4,50 @@ import "./App.css";
 
 function App() {
   const [points, setPoints] = useState([]);
+  const [undonePoints, setUndonePoints] = useState([]);
 
   function handlePlaceCircle(e) {
-    let point = { x: e.pageX - 4, y: e.pageY - 13 };
+    let point = { x: e.pageX - 2, y: e.pageY - 2 };
     console.log(point);
     setPoints([...points, point]);
   }
+  function handleUndo() {
+    // Remove the last point from points
+    let undo = points.pop();
+    setPoints([...points]);
+    setUndonePoints([...undonePoints, undo]);
+  }
+  function handleRedo() {
+    // Redo last undone point from undonePoints
+    let redo = undonePoints.pop();
+    setPoints([...points, redo]);
+    setUndonePoints([...undonePoints]);
+  }
 
   return (
-    <div className="App" onClick={handlePlaceCircle}>
-      <div>Hallo</div>
-      {points.map((point) => (
-        <div className="point" style={{ left: point.x, top: point.y }}>
-          o
-        </div>
-      ))}
+    <div>
+      <button
+        onClick={handleUndo}
+        disabled={points.length === 0 ? true : false}
+      >
+        Undo
+      </button>
+      <button
+        onClick={handleRedo}
+        disabled={undonePoints.length === 0 ? true : false}
+      >
+        Redo
+      </button>
+      <div className="App" onClick={handlePlaceCircle}>
+        <div>Hallo</div>
+        {points.map((point, idx) => (
+          <div
+            className="point"
+            style={{ left: point.x, top: point.y }}
+            key={idx}
+          ></div>
+        ))}
+      </div>
     </div>
   );
 }
