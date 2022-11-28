000d9839218d5cffe10f5608ef235fabc5ef1458
rroell <roelliie@hotmail.nl>
2022-11-28 01:02:35 +0100

    Base version

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import logo from "./logo.svg";
+import React from "react";
+import { useState } from "react";
 import "./App.css";
 
-function handlePlaceCircle(e) {
-  console.log(e);
-}
 function App() {
+  const [points, setPoints] = useState([]);
+
+  function handlePlaceCircle(e) {
+    let point = { x: e.pageX - 4, y: e.pageY - 13 };
+    console.log(point);
+    setPoints([...points, point]);
+  }
+
   return (
     <div className="App" onClick={handlePlaceCircle}>
-      Hallo
+      <div>Hallo</div>
+      {points.map((point) => (
+        <div className="point" style={{ left: point.x, top: point.y }}>
+          o
+        </div>
+      ))}
     </div>
   );
 }
